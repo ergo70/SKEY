4e3d43b9561b11cb0ffd50a2fb36a879e8d4c708
Ernst-Georg Schmid <pgchem@tuschehund.de>
2017-01-01 18:24:36 +0100

    Apache License

--- pam_skey/pam_skey.c
+++ pam_skey/pam_skey.c
@@ -1,8 +1,24 @@
+// PAM module for S/KEY-factor authentication.
+//
+// Copyright 2016 Ernst-Georg Schmid
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 /* Define which PAM interfaces we provide */
-#define PAM_SM_ACCOUNT
+//#define PAM_SM_ACCOUNT
 #define PAM_SM_AUTH
-#define PAM_SM_PASSWORD
-#define PAM_SM_SESSION
+//#define PAM_SM_PASSWORD
+//#define PAM_SM_SESSION
 
 #include <stdio.h>
 #include <stdlib.h>
