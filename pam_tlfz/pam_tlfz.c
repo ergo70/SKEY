5740fdb799c88da711f75a2b3290708ffe4a36f6
Ernst-Georg Schmid <pgchem@tuschehund.de>
2017-01-06 07:16:02 +0100

    Update pam_tlfz.c
    ISC License

--- pam_tlfz/pam_tlfz.c
+++ pam_tlfz/pam_tlfz.c
@@ -15,6 +15,26 @@
 //LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
 //OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 //PERFORMANCE OF THIS SOFTWARE.
+//
+//
+// The log_message, setuser, setgroup, drop_privileges, converse and request_pass
+// functions were taken from google-authenticator-libpam under the Apache-2.0
+// license.
+//
+// Copyright 2010 Google Inc.
+// Author: Markus Gutschke
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 /* Define which PAM interfaces we provide */
 //#define PAM_SM_ACCOUNT
